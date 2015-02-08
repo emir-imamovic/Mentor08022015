535cbff4dcb2849399c38ebd82ce2ae168a8f9af
Emir Imamovic <emir92@outlook.com>
2015-02-09 00:07:32 +0100

    "Initial commit"
    VideoStore

---
+++ src/RunVideoStore/Run.java
@@ -0,0 +1,11 @@
+package RunVideoStore;
+
+import Interface.GraphicUserInterface;
+
+public class Run {
+	public static void main(String[] args) {
+		GraphicUserInterface videoStore = new GraphicUserInterface(400, 100);
+
+	}
+
+}
